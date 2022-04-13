985d750b4526baa5150d272dc88430ab0edbc9c7
RusterX16 <EliottRuster@gmail.com>
2022-04-13 10:26:43 +0200

    Changing class Lore.java

--- src/main/java/dev/ruster/Lore.java
+++ src/main/java/dev/ruster/Lore.java
@@ -5,34 +5,34 @@ import java.util.*;
 public class Lore {
 
     private final ItemBuilder builder;
-    private final List<String> lore = new ArrayList<>();
+    private final List<String> loreList = new ArrayList<>();
 
     public Lore(ItemBuilder builder) {
         this.builder = builder;
     }
 
     public Lore set(Collection<String> lore) {
-        this.lore.addAll(lore);
+        this.loreList.addAll(lore);
         return this;
     }
 
     public Lore add(String... lines) {
-        Collections.addAll(lore, lines);
+        Collections.addAll(loreList, lines);
         return this;
     }
 
     public Lore remove(String... lines) {
-        Arrays.stream(lines).forEach(lore::remove);
+        Arrays.stream(lines).forEach(loreList::remove);
         return this;
     }
 
     public Lore clear() {
-        lore.clear();
+        loreList.clear();
         return this;
     }
 
     public List<String> toList() {
-        return lore;
+        return loreList;
     }
 
     public ItemBuilder builder() {
