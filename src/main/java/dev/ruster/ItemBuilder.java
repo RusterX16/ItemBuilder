58255b04199846438e4f5b1568a2b39bc77d08bc
RusterX16 <EliottRuster@gmail.com>
2022-05-28 02:04:42 +0200

    Some methods. Move to PC

--- src/main/java/dev/ruster/ItemBuilder.java
+++ src/main/java/dev/ruster/ItemBuilder.java
@@ -1,23 +1,27 @@
 package dev.ruster;
 
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ItemBuilder {
 
     private final ItemStack item;
     private final ItemMeta meta;
+    private final Damageable damageable;
     private final List<String> lore = new ArrayList<>();
+    private final Set<ItemFlag> flags = new HashSet<>();
+    private final Map<Enchantment, Integer> enchantments = new HashMap<>();
     private Material material;
     private String displayName;
     private int amount;
+    private short durability;
 
     /**
      * Create a new ItemBuilder
@@ -26,9 +30,23 @@ public class ItemBuilder {
      */
     public ItemBuilder(ItemStack item) {
         this.item = new ItemStack(item);
-        meta = item.getItemMeta();
         material = item.getType();
         amount = item.getAmount();
+        durability = item.getDurability();
+        enchantments.putAll(item.getEnchantments());
+        meta = item.getItemMeta();
+        damageable = (Damageable) meta;
+
+        if(item.hasItemMeta()) {
+            if(meta.hasDisplayName()) {
+                displayName = meta.getDisplayName();
+            }
+            if(meta.hasLore()) {
+                lore.addAll(meta.getLore());
+            }
+            flags.addAll(meta.getItemFlags());
+            durability = (short) ((Damageable) meta).getDamage();
+        }
     }
 
     /**
@@ -40,6 +58,7 @@ public class ItemBuilder {
     public ItemBuilder(Material material, int amount) {
         item = new ItemStack(material, amount);
         meta = item.getItemMeta();
+        damageable = (Damageable) meta;
         this.material = material;
         this.amount = amount;
     }
@@ -61,15 +80,21 @@ public class ItemBuilder {
     public ItemBuilder(@NotNull ItemBuilder builder) {
         item = new ItemStack(builder.item);
         meta = item.getItemMeta();
+        damageable = (Damageable) meta;
         material = builder.material;
         displayName = builder.displayName;
         amount = builder.amount;
+        durability = builder.durability;
+        lore.addAll(builder.lore);
+        flags.addAll(builder.flags);
+        enchantments.putAll(builder.enchantments);
     }
 
     /**
      * Change the material of the item
      *
      * @param material The material to set
+     * @return The ItemBuilder
      */
     public ItemBuilder material(Material material) {
         this.material = material;
@@ -81,6 +106,7 @@ public class ItemBuilder {
      * Set a new display name to the item
      *
      * @param displayName The name to display
+     * @return The ItemBuilder
      */
     public ItemBuilder displayName(String displayName) {
         this.displayName = displayName;
@@ -93,6 +119,7 @@ public class ItemBuilder {
      * overrides existing lore
      *
      * @param lore A collection of lines to set as lore
+     * @return The ItemBuilder
      */
     public ItemBuilder lore(List<String> lore) {
         this.lore.addAll(lore);
@@ -105,6 +132,7 @@ public class ItemBuilder {
      * overrides existing lore
      *
      * @param lines the lines set as lore
+     * @return The ItemBuilder
      */
     public ItemBuilder lore(String... lines) {
         this.lore.addAll(List.of(lines));
@@ -112,12 +140,26 @@ public class ItemBuilder {
         return this;
     }
 
+    /**
+     * Add a new line to the item lore
+     *
+     * @param line The text to append
+     * @return The ItemBuilder
+     */
     public ItemBuilder appendLoreLine(String line) {
         lore.add(line);
         meta.setLore(lore);
         return this;
     }
 
+    /**
+     * Define a new line in the lore on the given index.<br>
+     *
+     * @param index    The index where to set the line
+     * @param line     The text to display on lore
+     * @param override <br><b>true</b> : replace existing line<br><b>false</b> : shifts the next elements
+     * @return The ItemBuilder
+     */
     public ItemBuilder setLoreLine(int index, String line, boolean override) {
         if(override) {
             lore.set(index, line);
@@ -128,19 +170,132 @@ public class ItemBuilder {
         return this;
     }
 
+    /**
+     * Define a new line in the lore on the given index.<br>
+     * Will override the current line if existing
+     *
+     * @param index The index where to set the line
+     * @param line  The text to display on lore
+     * @return The ItemBuilder
+     */
     public ItemBuilder setLoreLine(int index, String line) {
-        return setLoreLine(index, line, false);
+        return setLoreLine(index, line, true);
     }
 
+    /**
+     * Remove a line of the lore by the index of where the line is
+     *
+     * @param index The index of the line to delete
+     * @return The ItemBuilder
+     */
     public ItemBuilder removeLoreLine(int index) {
         lore.remove(index);
         meta.setLore(lore);
         return this;
     }
 
+    /**
+     * Remove the given line of the lore
+     *
+     * @param line The line to delete
+     * @return The ItemBuilder
+     */
     public ItemBuilder removeLoreLine(String line) {
         lore.removeAll(Collections.singleton(line));
         meta.setLore(lore);
+        return this;
+    }
+
+    /**
+     * Clear the lore
+     *
+     * @return The ItemBuilder
+     */
+    public ItemBuilder clearLore() {
+        lore.clear();
+        meta.setLore(lore);
+        return this;
+    }
+
+    /**
+     * remove ItemFlag from the item
+     *
+     * @param flags The flag to hide
+     * @return The ItemBuilder
+     */
+    public ItemBuilder hideFlag(ItemFlag... flags) {
+        this.flags.addAll(List.of(flags));
+        meta.addItemFlags(flags);
+        return this;
+    }
+
+    /**
+     * Hide all ItemFlags from the item
+     *
+     * @return The ItemBuilder
+     */
+    public ItemBuilder hideFlags() {
+        return hideFlag(ItemFlag.values());
+    }
+
+    /**
+     * Add ItemFlag to the item
+     *
+     * @param flags The flag to show
+     * @return THe ItemBuilder
+     */
+    public ItemBuilder showFlag(ItemFlag... flags) {
+        List.of(flags).forEach(this.flags::remove);
+        meta.removeItemFlags(flags);
+        return this;
+    }
+
+    /**
+     * Show all ItemFlags on the item
+     *
+     * @return The ItemBuilder
+     */
+    public ItemBuilder showFlags() {
+        return showFlag(ItemFlag.values());
+    }
+
+    public ItemBuilder enchantment(Map<Enchantment, Integer> enchantments) {
+        this.enchantments.putAll(enchantments);
+        enchantments.forEach((e, i) -> meta.addEnchant(e, i, true));
+        return this;
+    }
+
+    public ItemBuilder addEnchantment(Enchantment enchantment, int level) {
+        enchantments.put(enchantment, level);
+        meta.addEnchant(enchantment, level, true);
+        return this;
+    }
+
+    public ItemBuilder removeEnchantment(Enchantment enchantment, int level) {
+        enchantments.entrySet().stream()
+                .filter(e -> e.getValue() == level)
+                .forEach(e -> {
+                    enchantments.remove(enchantment, level);
+                    meta.removeEnchant(e.getKey());
+                });
+        return this;
+    }
+
+    public ItemBuilder removeEnchantments(Enchantment... enchantments) {
+        List.of(enchantments).forEach(e -> {
+            this.enchantments.remove(e);
+            meta.removeEnchant(e);
+        });
+        return this;
+    }
+
+    public ItemBuilder removeEnchantments(int @NotNull ... levels) {
+        enchantments.forEach((key, value) -> Arrays.stream(levels)
+                .filter(l -> value == l)
+                .forEach(l -> {
+                    enchantments.remove(key);
+                    meta.removeEnchant(key);
+                }));
         return this;
     }
 
@@ -156,13 +311,91 @@ public class ItemBuilder {
     }
 
     /**
+     * Set a durability on the item.
+     *
+     * @param durability The new durability of the item
+     * @return The ItemBuilder
+     */
+    public ItemBuilder durability(short durability) {
+        this.durability = durability;
+        damageable.setDamage(durability);
+        return this;
+    }
+
+    /**
+     * Damage the item. The value given will be subtracted to the current item life.
+     *
+     * @param damage The damage to deal
+     * @return The ItemBuilder
+     */
+    public ItemBuilder damage(short damage) {
+        return durability((short) (item.getMaxItemUseDuration() - damage));
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        ItemBuilder builder = (ItemBuilder) obj;
+        return builder.item.equals(item) &&
+                builder.meta.equals(meta) &&
+                builder.material.equals(material) &&
+                builder.displayName.equals(displayName) &&
+                builder.amount == amount &&
+                builder.durability == durability &&
+                builder.lore.equals(lore) &&
+                builder.flags.equals(flags) &&
+                builder.enchantments.equals(enchantments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(item, meta, lore, flags, enchantments, material, displayName, amount, durability);
+    }
+
+    /**
      * Build the item
      *
      * @return The ItemStack
      */
     public ItemStack build() {
-        item.setItemMeta(meta);
+        item.setItemMeta(damageable);
         return item;
+    }
+
+    /**
+     * Get the instance of the ItemBuilder by a given ItemStack
+     *
+     * @param item The ItemStack to get from
+     * @return The ItemBuilder instance
+     */
+    public ItemBuilder getFromItemStack(@NotNull ItemStack item) {
+        if(item.isSimilar(this.item)) {
+            return this;
+        }
+        return null;
+    }
+
+    /**
+     * Get the ItemStack
+     *
+     * @return The ItemStack
+     */
+    public ItemStack getItemStack() {
+        return item;
+    }
+
+    /**
+     * Get the ItemMeta
+     *
+     * @return The ItemMeta
+     */
+    public ItemMeta getMeta() {
+        return meta;
     }
 
     /**
@@ -193,12 +426,39 @@ public class ItemBuilder {
     }
 
     /**
+     * Get the item durability
+     *
+     * @return The durability
+     */
+    public short getDurability() {
+        return durability;
+    }
+
+    /**
      * Get the item lore
      *
      * @return The lore
      */
     public List<String> getLore() {
         return lore;
+    }
+
+    /**
+     * Get the item flags
+     *
+     * @return The flags
+     */
+    public Set<ItemFlag> getFlags() {
+        return flags;
+    }
+
+    /**
+     * Get the state of the item flags
+     *
+     * @return The state of the item flags<br><b>true</b> if item flags are hidden<br><b>false</b> if item flags are visible
+     */
+    public boolean hasFlags() {
+        return !flags.isEmpty();
     }
 
     @Override
